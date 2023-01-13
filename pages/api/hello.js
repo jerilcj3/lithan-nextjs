f2abc7700b4d8748daa37046d37426f2f4f18320
jerilcj3 <jerilcj3@gmail.com>
2023-01-13 10:46:54 +0530

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const Mautic = require('mautic')
+import requestIp from 'request-ip'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+const client = new Mautic({
+  baseUrl: 'https://mautic.educlaas.com/api',
+  auth: {
+    username: 'educlaas',
+    password: 'educlaas*2@3$1#'
+  }
+})
+
+
+export default async function handler(req, res) {
+  const detectedIp = requestIp.getClientIp(req)
+  const response = await client.contacts.list({ search: `ip: ${detectedIp}` })
+  const contactFields = Object.values(response.data.contacts)[0].fields  
+  res.status(200).json({ title: contactFields.all.title, firstname: contactFields.all.firstname, email: contactFields.all.email, phone: contactFields.all.phone, country: contactFields.all.country  })
 }
